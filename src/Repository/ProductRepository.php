cdf351eec9fa259b8ccf0a672b01e2942e10c5f7
Pedro Schinke <pedro.schinke@outlook.com>
2024-08-01 00:48:59 -0300

    added new ProductRepository methods

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -14,15 +14,15 @@ class ProductRepository
         $this->pdo = $pdo;
     }
 
-    public function formObject($product)
+    public function formObject($productData)
     {
         return new Product(
-            $product['id'],
-            $product['type'],
-            $product['name'],
-            $product['description'],
-            $product['image'],
-            $product['price']
+            $productData['id'],
+            $productData['type'],
+            $productData['name'],
+            $productData['description'],
+            $productData['price'],
+            $productData['image']
         );
     }
 
@@ -56,9 +56,22 @@ class ProductRepository
         return $lunchArray;
     }
 
+    public function getProduct(int $id)
+    {
+        $sql = "SELECT * FROM produtos WHERE id = ?;";
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(1, $id);
+        $statement->execute();
+        $productData = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $productAsObject = $this->formObject($productData);
+
+        return $productAsObject;
+    }
+
     public function getAllProducts(): array
     {
-        $sql = "SELECT * FROM produtos;";
+        $sql = "SELECT * FROM produtos ORDER BY price;";
         $statement = $this->pdo->prepare($sql);
         $statement->execute();
         $products = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -68,5 +81,41 @@ class ProductRepository
         }, $products);
 
         return $productsArray;
+    }
+
+    public function deleteProduct(int $id): void
+    {
+        $sql = "DELETE FROM produtos WHERE id = ?;";
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(1, $id);
+        $statement->execute();
+    }
+
+    public function editProduct(Product $product)
+    {
+        $sql = "UPDATE produtos
+                SET name = ?, type = ?, description = ?, price = ?, image = ?
+                WHERE id = ?";
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(1, $product->getName()); 
+        $statement->bindValue(2, $product->getType()); 
+        $statement->bindValue(3, $product->getDescription()); 
+        $statement->bindValue(4, $product->getPrice()); 
+        $statement->bindValue(5, $product->getImage()); 
+        $statement->bindValue(6, $product->getId()); 
+        $statement->execute();
+    }
+
+    public function save(Product $product)
+    {
+        $sql = "INSERT INTO produtos (type, name, description, image, price) 
+                VALUES (?, ?, ?, ?, ?);";
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(1, $product->getType());
+        $statement->bindValue(2, $product->getName());
+        $statement->bindValue(3, $product->getDescription());
+        $statement->bindValue(4, $product->getImage());
+        $statement->bindValue(5, $product->getPrice());
+        $statement->execute();
     }
 }
